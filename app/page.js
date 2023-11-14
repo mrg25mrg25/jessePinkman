dc7e498a95b72174cd3bb680ad3f7a5287d71db6
vaggoban <143816541+vaggoban@users.noreply.github.com>
2023-11-14 20:03:51 +0600

    impossible

--- app/page.js
+++ app/page.js
@@ -1,9 +1,28 @@
-import React from 'react'
 
-const page = () => {
-  return (
-    <div>page</div>
-  )
+
+function udemy (props){
+  const { products } = props;
+return (
+<ul>
+{products.map((product) => (
+         <li key={product.id}> {product.title} </li>
+      ))}
+</ul>
+)
+
 }
 
-export default page
+export async function getStaticProps() {
+return { 
+props: {
+ products: [{id:'p1',title:'product 1'}],
+},
+};
+}
+
+
+
+
+
+export default udemy;
+
